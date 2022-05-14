b4004c470c3c5a15f744f1d38d54ca8c2905e78b
Scnmbag <a@scnmbag.cc>
2022-05-14 18:41:42 +0300

    Rework some functions in main.go file
    The CRUDL functions now return an error value.

--- main.go
+++ main.go
@@ -22,41 +22,37 @@ type dbUserEntity struct {
 var dataBaseURL string = "postgres://login:pass@localhost:5432/database-name"
 
 func main() {
-	db := OpenDB(dataBaseURL)
+	db, err := sql.Open("pgx", dataBaseURL)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer db.Close()
 }
 
-func OpenDB(url string) *sql.DB {
-	db, err := sql.Open("pgx", url)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return db
-}
-
-func Create(data dbUserEntity, dataBase *sql.DB) {
+func Create(data dbUserEntity, dataBase *sql.DB) error {
 	uuid := uuid.New()
-	//time := time.Now() использую now() в postgreSQL
-	_, err := dataBase.Exec("INSERT INTO users(id, name, phone, email, created_at) VALUES ($1, $2, $3, $4, now())", uuid, data.name, data.phoneNumber, data.email)
+	currentTime := time.Now()
+	_, err := dataBase.Exec("INSERT INTO users(id, name, phone, email, created_at) VALUES ($1, $2, $3, $4, $5)", uuid, data.name, data.phoneNumber, data.email, currentTime)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	fmt.Println("Entry have been created")
+	fmt.Println("Row has been created")
+	return nil
 }
 
-//какой тип использовать для параметра id? uuid.UUID or [16]byte?
-func GetById(id [16]byte, dataBase *sql.DB) {
+func GetById(id uuid.UUID, dataBase *sql.DB) error {
 	var user dbUserEntity
 	row := dataBase.QueryRow("SELECT * FROM users WHERE id = $1", id)
 	err := row.Scan(&user.id, &user.name, &user.phoneNumber, &user.email, &user.createdDate)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	fmt.Printf("%-36s %-10s %-15s %-20s %s\n", "id", "name", "phone", "email", "created_at")
 	fmt.Printf("%-36s %-10s %-15s %-20s %v\n", user.id, user.name, user.phoneNumber, user.email, user.createdDate)
+	return nil
 }
 
-func UpdateById(id [16]byte, fields map[string]string, dataBase *sql.DB) {
+func UpdateById(id uuid.UUID, fields map[string]string, dataBase *sql.DB) error {
 	var (
 		updateString string
 		queryText    string
@@ -75,65 +71,33 @@ func UpdateById(id [16]byte, fields map[string]string, dataBase *sql.DB) {
 	queryText = "UPDATE users SET " + updateString + " WHERE id = $1"
 	_, err := dataBase.Exec(queryText, id)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	fmt.Println("Row have been updated")
+	fmt.Println("Row has been updated")
+	return nil
 }
 
-func DeleteById(id [16]byte, dataBase *sql.DB) {
+func DeleteById(id uuid.UUID, dataBase *sql.DB) error {
 	_, err := dataBase.Exec("DELETE FROM users WHERE id = $1", id)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	fmt.Println("Row have been deleted")
+	fmt.Println("Row has been deleted")
+	return nil
 }
 
-func List(dataBase *sql.DB, limit int) {
+func List(dataBase *sql.DB, limit ...any) error {
 	var limitString string
 	var user dbUserEntity
-	if limit > 0 {
-		limitString = " LIMIT " + strconv.Itoa(limit)
-	}
-	rows, err := dataBase.Query("SELECT * FROM users" + limitString)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	rowsNames, err := rows.Columns()
-	if err != nil {
-		log.Print(err)
-	}
-	fmt.Printf("%-36s %-10s %-15s %-20s %s\n", rowsNames[0], rowsNames[1], rowsNames[2], rowsNames[3], rowsNames[4])
-	for rows.Next() {
-		rows.Scan(&user.id, &user.name, &user.phoneNumber, &user.email, &user.createdDate)
-		fmt.Printf("%-36s %-10s %-15s %-20s %v\n", user.id, user.name, user.phoneNumber, user.email, user.createdDate)
-	}
-}
-
-func List1(a ...any) {
-	var (
-		dataBase      *sql.DB
-		limitQuantity int
-		limitString   string
-		user          dbUserEntity
-	)
-	for _, v := range a {
-		switch v := v.(type) {
-		case *sql.DB:
-			dataBase = v
-		case int:
-			limitQuantity = v
+	for _, v := range limit {
+		limitNumber, ok := v.(int)
+		if ok && limitNumber > 0 {
+			limitString = " LIMIT " + strconv.Itoa(limitNumber)
 		}
 	}
-	if dataBase == nil {
-		log.Fatal("This function must take a *sql.DB to work")
-	}
-	if limitQuantity > 0 {
-		limitString = " LIMIT " + strconv.Itoa(limitQuantity)
-	}
 	rows, err := dataBase.Query("SELECT * FROM users" + limitString)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer rows.Close()
 	rowsNames, err := rows.Columns()
@@ -145,4 +109,5 @@ func List1(a ...any) {
 		rows.Scan(&user.id, &user.name, &user.phoneNumber, &user.email, &user.createdDate)
 		fmt.Printf("%-36s %-10s %-15s %-20s %v\n", user.id, user.name, user.phoneNumber, user.email, user.createdDate)
 	}
+	return nil
 }
